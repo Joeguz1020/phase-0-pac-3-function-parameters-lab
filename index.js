4d3ba763f63e876161afcba9dda773fbc8894398
Joseph Guzman <joe.guzman614@yahoo.com>
2022-05-24 21:13:44 -0400

    commit tree 7cdcbe43b490e0b900b4136bb334a3cac0eba847

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction(Aki) { 
+    console.log(Aki);
+    return (`Hi, my name is ${Aki}.`);
+}
+function introductionWithLanguage(Aki, Ember.js) {
+    console.log(Aki, Ember.js);
+    return ( `Hi, my name is ${Aki} and I am learning to program in ${Java}.`);
+}
